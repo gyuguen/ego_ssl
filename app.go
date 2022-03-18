696d78d3a112ad1c1aa6a11952bbeef027cac163
gyuguen.jang <gyuguen.jang@medibloc.org>
2022-03-18 14:13:25 +0900

    fix

--- app.go
+++ app.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	res, err := http.Get("https://json-schema.org/draft/2020-12/schema")
+	tlsConfig := &tls.Config{InsecureSkipVerify: true}
+	client := http.Client{
+		Transport: &http.Transport{TLSClientConfig: tlsConfig}}
+	res, err := client.Get("https://json-schema.org/draft/2020-12/schema")
 	if err != nil {
 		fmt.Println(fmt.Errorf("failed get https url: %w", err))
 		panic(err)
